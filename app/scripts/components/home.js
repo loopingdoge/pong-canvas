1acb3e344a738d83abde25475f0ce0e7be2c9f27
albyxyz <albyx.n@gmail.com>
2016-01-25 00:57:55 +0100

    framerate

--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Ball from './ball';
+import Rx from 'rx';
 
 var Home = React.createClass({
 
+	getInitialState() {
+    return {
+      framerate: Rx.Observable.interval(16).timeInterval()
+    };
+  },
+
+	componentDidMount() {
+
+	},
+
   render() {
     return (
-      <Ball canvasId="canvas"/>
+      <Ball canvasId="canvas" framerate={this.state.framerate}/>
     );
   }
 });
