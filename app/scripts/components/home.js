c9344bffa20fd56721f6b33458b53e821e6d8fd6
kdpkke kdpkke <kdpkke kdpkke>
2016-01-25 01:05:37 +0100

    field.js added on home.js

--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Ball from './ball';
-
+import Field from './field';
 var Home = React.createClass({
 
   render() {
     return (
       <Ball canvasId="canvas"/>
+      <Field canvasId="field"/>
     );
   }
 });
