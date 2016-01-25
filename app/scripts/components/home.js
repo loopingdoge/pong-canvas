64cbea3e2aa500204d420fb4f7bd52573cce0793
albyxyz <albyx.n@gmail.com>
2016-01-25 02:10:01 +0100

    ball moving and framerate structure

--- app/scripts/components/home.js
+++ app/scripts/components/home.js
@@ -6,18 +6,30 @@ var Home = React.createClass({
 
 	getInitialState() {
     return {
-      framerate: Rx.Observable.interval(16).timeInterval()
+      canvas: document.getElementById('canvas'),
+      ctx: canvas.getContext("2d"),
+      framerate: Rx.Observable.interval(16).timeInterval(),
+      drawables: []
     };
   },
 
 	componentDidMount() {
-
+    const framerate = this.state.framerate;
+    const drawables = [];
+    drawables.push(Ball());
+    this.setState({drawables: drawables})
+    framerate.subscribe(this.drawAll);
 	},
 
+  drawAll() {
+    this.state.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.state.drawables.map((drawable) => {
+      drawable.draw();
+    });
+  },
+
   render() {
-    return (
-      <Ball canvasId="canvas" framerate={this.state.framerate}/>
-    );
+    return null;
   }
 });
 
